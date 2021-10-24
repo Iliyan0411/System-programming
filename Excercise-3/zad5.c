498d7761a197d3115ab9a46327d4737604143a6a
Iliyan0411 <ilian.dd00@gmail.com>
2021-10-24 12:40:32 +0300

    zad5.c - optimized

--- Excercise-3/zad5.c
+++ Excercise-3/zad5.c
@@ -7,62 +7,69 @@
 
 int main(int argc, char **argv)
 {
-    int fd1 = open(argv[1], O_RDONLY);
-
-    int size = lseek(fd1, 0, SEEK_END);
-    lseek(fd1, 0, SEEK_SET);
-
-    char *text = (char *)calloc(size+1, sizeof(char));
-    read(fd1, text, size);
-
-    int i = 0, j = 0, newLines = 0;
-    while(i < size)
-    {
-        char *line = (char *)calloc(61, sizeof(char));
-        while(i < size && text[i] != '\n')
+        int fd1;
+        if((fd1 = open(argv[1], O_RDONLY)) == -1)
         {
-            line[j] = text[i];
-            j++;
-            i++;
+                printf("%s\n", "An error has occured!");
+                return -1;
         }
-        line[j] = '\0';
-        if(line[j-1] == '$')
-        {
-                write(1, line, j);
-                printf("%c", '\n');
-        }
-        else
-        {
-                int fd2 = open(argv[2], O_WRONLY | O_CREAT | O_APPEND, 0644);
 
-                char *digits = (char *)calloc(j, sizeof(char));
-                int pos = 0;
-                for(int k = 0; k < j; ++k)
+        int size = lseek(fd1, 0, SEEK_END);
+        lseek(fd1, 0, SEEK_SET);
+
+        char *text = (char *)calloc(size+1, sizeof(char));
+        read(fd1, text, size);
+
+        int i = 0, j = 0, newLines = 0;
+        while(i < size)
+        {
+                char *line = (char *)calloc(61, sizeof(char));
+                while(i < size && text[i] != '\n')
                 {
-                        if(line[k] >= '0' && line[k] <= '9')
-                        {
-                                digits[pos] = line[k];
-                                pos++;
-                        }
+                        line[j++] = text[i++];
                 }
-                digits[pos] = '\n';
-                digits[pos+1] = '\0';
-                write(fd2, digits, pos);
-                newLines++;
+                line[j] = '\0';
+                if(line[j-1] == '$')
+                {
+                        write(1, line, j);
+                        printf("%c", '\n');
+                }
+                else
+                {
+                        int fd2;
+                        if((fd2 = open(argv[2], O_WRONLY | O_CREAT | O_APPEND, 0644)) == -1)
+                        {
+                                printf("%s\n", "An error has occured!");
+                                return -1;
+                        }
 
-                free(digits);
-                close(fd2);
+                        char *digits = (char *)calloc(j, sizeof(char));
+                        int pos = 0;
+                        for(int k = 0; k < j; ++k)
+                        {
+                                if(line[k] >= '0' && line[k] <= '9')
+                                {
+                                        digits[pos++] = line[k];
+                                }
+                        }
+                        digits[pos++] = '\n';
+                        digits[pos] = '\0';
+                        write(fd2, digits, pos);
+                        newLines++;
+
+                        free(digits);
+                        close(fd2);
+                }
+                j = 0;
+                i++;
+                free(line);
         }
-        j = 0;
-        i++;
-        free(line);
-    }
 
-    printf("%d\n", newLines);
+        printf("%d\n", newLines);
 
-    close(fd1);
-    free(text);
+        close(fd1);
+        free(text);
 
 
-    return 0;
+        return 0;
 }
